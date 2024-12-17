753d42002ffdb3e6a77f5744a2e980d0d8684201
Angelo Basile <92684587+carni123@users.noreply.github.com>
2024-12-17 11:49:21 +0000

    MANCA IL CURSOR

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-app.run()
+app.config["MYSQL_HOST"]="138.41.20.102"
+app.config["MYSQL_PORT"]="53306"
+app.config["MYSQL_USER"]="ospite"
+app.config["MYSQL_PASSWORD"]="ospite"
+app.config["MYSQL_DB"]="w3schools"
+mysql=MySQL()
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/prodotti")
+def prodotti():
+    return render_template("prodotti.html",prodotti=)
+
+app.run(debug=True)
